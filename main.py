80651079275dd3e81cb2a95ea209079b170de5ba
Arken <110853531+Arken-Lunk@users.noreply.github.com>
2022-08-10 12:08:02 +0200

    I'm stupid (also added explanations in Spanish)

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ import asyncio
 import keep_alive
 from functools import partial
 
-# Get the API token from the .env file.
+#Prepara el bot indicando el prefijo.
 intents = discord.Intents().all()
 client = discord.Client(intents=intents)
 bot = commands.Bot(command_prefix='Por favor ', intents=intents)
 
+#Configuraciones de yt_dlp y de ffmpeg.
 yt_dlp.utils.bug_reports_message = lambda: ''
 ytdl_format_options = {
     'format': 'bestaudio/best',
@@ -23,7 +24,7 @@ ytdl_format_options = {
     'no_warnings': True,
     'default_search': 'auto',
     'source_address':
-    '0.0.0.0'  # bind to ipv4 since ipv6 addresses cause issues sometimes
+    '0.0.0.0'
 }
 ffmpeg_options = {
     'options': '-vn',
@@ -32,40 +33,44 @@ ffmpeg_options = {
 }
 ytdl = yt_dlp.YoutubeDL(ytdl_format_options)
 
+#Prepara la cola
 queue = asyncio.Queue()
-dict = {}
 
-
+#Estas dos funciones son el loop que irá mirando la cola y reproduciendo las canciones.
+#La primera simplemente mira si hay algo en la cola y llama a la segunda.
 async def check_queue(ctx):
     if queue.qsize() > 0:
         await player_loop(ctx)
 
-
+#Esta función se ocupa de todo.
 async def player_loop(ctx):
-    """Our main player loop."""
     await bot.wait_until_ready()
     while not bot.is_closed():
-        try:
+            #Primero, conseguimos la información del servidor y de la fuente extraídas de la cola.
             origin = await queue.get()
-            for key, val in origin.items():
+            try:
+              for key, val in origin.items():
                 guild = bot.get_guild(key)
                 source = val
+            except:
+              guild = ctx.guild
+              source = origin
+            
+            #Luego, lo reproducimos.
             async with ctx.typing():
                 voice = get(bot.voice_clients, guild=guild)
-                URL = source.get('url')
+                URL = source[0]
                 try:
                     voice.play(discord.FFmpegOpusAudio(URL, **ffmpeg_options),
                                after=lambda _: (await check_queue(ctx)
                                                 for _ in '_').__anext__())
                 except:
+                    #Si falla porque ya hay otra canción en marcha, devolvemos la canción a la cola.
                     await queue.put(source)
                     continue
-            await ctx.send('**Reproduciendo:** {}'.format(source.get("title")))
-        except AttributeError:
-            await ctx.send(
-                "Por favor, vuélveme a decir que vaya al canal de voz.")
+            await ctx.send('**Reproduciendo:** {}'.format(source[1]))
 
-
+#Configuración de la fuente.
 class YTDLSource(discord.PCMVolumeTransformer):
     def __init__(self, source, *, data, volume=0.5):
         super().__init__(source, volume)
@@ -105,7 +110,7 @@ class YTDLSource(discord.PCMVolumeTransformer):
 async def on_ready():
     print("Ready!")
 
-
+#Comando bastante autoexplicatorio.
 @bot.command(name='ven', help='Haz que Guru-Guru se una al canal de voz.')
 async def join(ctx):
     if not ctx.message.author.voice:
@@ -117,6 +122,8 @@ async def join(ctx):
         channel = ctx.message.author.voice.channel
     await channel.connect()
     await ctx.send("¡Hola!")
+    
+    #Esto lo puse para ver si pillaba el servidor bien.
     print(ctx.guild)
 
 
@@ -129,35 +136,44 @@ async def leave(ctx):
     else:
         await ctx.send("Lo siento, no estoy en ningún canal.")
 
-
+#El infierno hecho comando.
 @bot.command(
     name='pon',
     help=
     '¡Escribe el título de tu canción deseada y Guru-Guru te la reproducirá! (IMPORTANTE: haz que se una al chat de voz antes de pedirle que reproduzca nada)'
 )
 async def play(ctx, *, url):
-    global dict
+    #Crea un diccionario.
+    dict = {}
+    
+    #Pilla la información del vídeo.
     with yt_dlp.YoutubeDL(ytdl_format_options) as ydl:
         info = ydl.extract_info(url, download=False)
     Entries = []
     for i in info['entries']:
         Entries.append(i)
-    dict[ctx.guild.id] = Entries[0]
+        
+    #Añade el enlace junto al título como value, y la key es la ID del servidor.
+    dict[ctx.guild.id] = [Entries[0].get('url'), Entries[0].get('title')]
+    
+    #Lo pone en la cola y lo reproduce.
     await queue.put(dict)
     await check_queue(ctx)
 
 
+#Igual que el anterior pero sin reproducirlo al final.
 @bot.command(
     name='añade',
     help='Añade una canción a la cola mientras otra se está reproduciendo.')
 async def add(ctx, *, url):
-    global dict
+    dict = {}
     with yt_dlp.YoutubeDL(ytdl_format_options) as ydl:
         info = ydl.extract_info(url, download=False)
     Entries = []
     for i in info['entries']:
         Entries.append(i)
-    dict[ctx.guild.id] = Entries[0]
+    dict[ctx.guild.id] = [Entries[0].get('url'), Entries[0].get('title')]
+    print(dict)
     await ctx.send("¡Canción {} añadida a la cola!".format(
         Entries[0].get("title")))
     await ctx.send("Hay {} canciones en la cola.".format(queue.qsize() + 1))
@@ -182,6 +198,7 @@ async def resume(ctx):
         await ctx.send("No tengo canción que resumir. ¡Pídeme que ponga otra!")
 
 
+#Vacía la cola y para la reproducción.
 @bot.command(
     name='para',
     help=
@@ -195,6 +212,7 @@ async def stop(ctx):
     if voice_client.is_playing():
         voice_client.stop()
         await ctx.send("¡Como digas!")
+        voice_client.stop()
     else:
         await ctx.send("Lo siento, no estoy tocando nada.")
 
@@ -209,19 +227,19 @@ async def next(ctx):
     else:
         await ctx.send("Lo siento, no estoy tocando nada.")
 
-
+#Esto aún no va bien.
 @bot.command(name="lista", help="Muestra la lista de canciones pendientes.")
 async def queue_info(ctx):
     a = []
     while not queue.empty():
         a.append(queue.get_nowait())
-        await ctx.send(a.get("title"))
+        await ctx.send(a[len(a)-1][1])
     for song in a:
         await queue.put(a)
 
-
+#Esta parte es para poder hostearlo en Replit.
 keep_alive.keep_alive()
 if __name__ == "__main__":
     bot.run(
-        "MTAwNDcwNDg2NjU4MjY3MTQ2MQ.GN82Q-.GZhHPTuvsWjl4GoGJIpBI8RH7hbUmY74ZPvIhU"
+        "token"
     )
